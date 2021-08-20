044ecef90354323670c87afea843f5a75724c12a
Arnaud DON <donnadille.arnaud@gmail.com>
2021-08-20 16:04:15 +0200

    Implementation de l'U.S. login - WIP
    A partir du tuto : https://www.bezkoder.com/angular-12-spring-boot-jwt-auth/ j'ai créé les components et service de login côté front.
    Reste à faire :
     - Le back
     - La mise en page
     - Adapter les différents éléments à notre application

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  form: any = {
+    mail: null,
+    password: null
+  };
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    const { mail, password } = this.form;
+
+    this.authService.register(mail, password).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
+  }
+}
