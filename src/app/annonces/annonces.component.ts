96ef4142ceb7ad1a1bfe438d812098f614667b53
Arnaud DON <donnadille.arnaud@gmail.com>
2021-08-28 21:12:41 +0200

    Handle redirections
    Redirect to login page if not logged in and to profile page if logged in as Chauffeur

--- src/app/annonces/annonces.component.ts
+++ src/app/annonces/annonces.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenStorageService } from '../services/token-storage.service';
 
 @Component({
   selector: 'app-annonces',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnnoncesComponent implements OnInit {
 
-  constructor() { }
+  currentUser: any;
+
+  constructor(private token: TokenStorageService, private router: Router) { }
 
   ngOnInit(): void {
+    this.currentUser = this.token.getUser();
+    if(!this.token.getToken()){
+      this.router.navigate(['/login'])
+        .then(() => {
+          window.location.reload();
+        });
+    }
+    else if(this.currentUser.role === "CHAUFFEUR"){
+      this.router.navigate(['/profile'])
+        .then(() => {
+          window.location.reload();
+        });
+    }
   }
 
 }
