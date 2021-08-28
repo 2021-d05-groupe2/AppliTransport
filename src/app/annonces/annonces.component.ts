05e16dd03ca908e60fddf53de9436c6f32ba6cd5
Arnaud DON <donnadille.arnaud@gmail.com>
2021-08-29 01:25:06 +0200

    Front of annonce publication form

--- src/app/annonces/annonces.component.ts
+++ src/app/annonces/annonces.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { Component, ElementRef, Inject, LOCALE_ID, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { TokenStorageService } from '../services/token-storage.service';
+declare var $ : any;
 
 @Component({
   selector: 'app-annonces',
@@ -10,10 +12,37 @@ import { TokenStorageService } from '../services/token-storage.service';
 export class AnnoncesComponent implements OnInit {
 
   currentUser: any;
+  @ViewChild('newAnnonceModal') newAnnonceModal: ElementRef;
 
-  constructor(private token: TokenStorageService, private router: Router) { }
+  constructor(private token: TokenStorageService, private router: Router, @Inject(LOCALE_ID) private locale: string) { }
+
+  heures = new Map();
+  minutes = new Map();
+  minDate:string;
 
   ngOnInit(): void {
+
+    //Mapping heures
+    for(let i = 0; i <= 24; i++){
+      let displayValue = ""+i;
+      if(i<10){
+        displayValue = "0"+displayValue;
+      }
+      this.heures.set(i,  displayValue);
+    }
+
+    //Mapping minutes
+    this.minutes.set(0, "00");
+    for(let i = 10; i < 60; i=i+10){
+      this.minutes.set(i,  i);
+    }
+
+    //Set date minimum
+    let nowPlusOneDay = new Date(Date.now());
+    nowPlusOneDay.setDate(nowPlusOneDay.getDate() + 1);
+    this.minDate = formatDate(nowPlusOneDay,'yyyy-MM-dd',this.locale);
+
+    // Roles redirections
     this.currentUser = this.token.getUser();
     if(!this.token.getToken()){
       this.router.navigate(['/login'])
@@ -27,6 +56,14 @@ export class AnnoncesComponent implements OnInit {
           window.location.reload();
         });
     }
+  }
+
+  public open() {
+    this.newAnnonceModal.nativeElement.style.display = 'block';
+  }
+
+  public close() {
+    this.newAnnonceModal.nativeElement.style.display = 'none';
   }
 
 }
