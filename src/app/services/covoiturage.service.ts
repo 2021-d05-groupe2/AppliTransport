23ad1b256dbc1250e8645fe92c1cdf31bc5a802a
Arnaud DON <donnadille.arnaud@gmail.com>
2021-09-01 07:50:58 +0200

    Update covoiturage services

--- src/app/services/covoiturage.service.ts
+++ src/app/services/covoiturage.service.ts
@@ -4,8 +4,7 @@ import { Observable } from 'rxjs';
 import { TokenStorageService } from './token-storage.service';
 import { Covoiturage } from '../model/covoiturage.model';
 
-const createUrl = 'http://localhost:8090/api/travel/newAnnonce';
-const api = 'http://localhost:8090/api';
+const apiTravel = 'http://localhost:8090/api/travel';
 @Injectable({
   providedIn: 'root'
 })
@@ -14,29 +13,33 @@ export class CovoiturageService {
   constructor(private http: HttpClient, private tokenStorageService: TokenStorageService) {
   }
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + this.tokenStorageService.getUser().token,
+      'Access-Control-Allow-Origin': 'http://localhost:4200'
+    })
+  };
+
   create(data: any): Observable<any> {
 
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + this.tokenStorageService.getUser().token
-      })
-    };
-    return this.http.post(createUrl, data, httpOptions);
+    return this.http.post(apiTravel + '/newAnnonce', data, this.httpOptions);
   }
 
   public getallCovoit(): Observable<Covoiturage[]>{
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + this.tokenStorageService.getUser().token,
-        'Access-Control-Allow-Origin': 'http://localhost:4200'
-      })
-    };
-    return this.http.get<Covoiturage[]>(api+'/travel/listcovoiturage/',httpOptions);
+    return this.http.get<Covoiturage[]>(apiTravel+'/listcovoiturage/',this.httpOptions);
   }
 
   public getcovoitencours(): Observable<Covoiturage[]>{
-    return this.http.get<Covoiturage[]>(api + '/travel/listcoivoiturageencours');
+    return this.http.get<Covoiturage[]>(apiTravel + '/listcoivoiturageencours',this.httpOptions);
+  }
+
+  public getAvailableCovoits(): Observable<Covoiturage[]>{
+    return this.http.get<Covoiturage[]>(apiTravel + '/listAvailablesCovoiturages',this.httpOptions);
+  }
+
+  public reserverCovoits(covoits:Covoiturage[]): Observable<Covoiturage[]>{
+
+    return this.http.post<Covoiturage[]>(apiTravel + '/reserverCovoits',this.httpOptions);
   }
 }
